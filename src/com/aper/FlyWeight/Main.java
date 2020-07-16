5a62f008417a5fac99673a47e040345fa270738c
sevenaper <lxsdwlhh@163.com>
2020-07-16 22:51:11 +0800

    add flyweight

--- src/com/aper/FlyWeight/Main.java
+++ src/com/aper/FlyWeight/Main.java
@@ -1,4 +1,47 @@
 package com.aper.FlyWeight;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+class Bullet {
+    public UUID id = UUID.randomUUID();
+    boolean living = true;
+
+    @Override
+    public String toString() {
+        return "Bullet{" +
+                "id=" + id +
+                ", living=" + living +
+                '}';
+    }
+}
+
+class BulletPool {
+    List<Bullet> bullets = new ArrayList<>();
+
+    {
+        for (int i = 0; i < 5; i++) {
+            bullets.add(new Bullet());
+        }
+    }
+
+    public Bullet getBullet() {
+        for (Bullet b : bullets) {
+            if (!b.living) {
+                return b;
+            }
+        }
+        return new Bullet();
+    }
+}
+
 public class Main {
+    public static void main(String[] args) {
+        BulletPool bulletPool = new BulletPool();
+        for (int i = 0; i < 10; i++) {
+            Bullet bullet = bulletPool.getBullet();
+            System.out.println(b);
+        }
+    }
 }
